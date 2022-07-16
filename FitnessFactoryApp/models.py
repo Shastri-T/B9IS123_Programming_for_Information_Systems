25914ac86ca7e7cdd459feea8a7aee5f8dfad424
shas556 <105118561+shas556@users.noreply.github.com>
2022-07-16 19:29:57 +0100

    installed djongo database adapter and dnspython module for connecting to atlas mongodb

--- FitnessFactoryApp/models.py
+++ FitnessFactoryApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    productID=models.AutoField(primary_key=True)
+    productName=models.CharField(max_length=20)
